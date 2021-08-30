454eed78732d0bddba7ec20f4ca8abfeeb760f05
gilbertoreact <gilbertoreact@gmail.com>
2021-08-30 00:16:13 -0300

    adicionando rotas

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Catalogo = () => {
-  return (
-    <div id="Div_Catalogo">
-    <h1>Catalogo</h1>
-    </div>
-  );
+    return (
+        <div id="Div_Catalogo">
+            <h1>Catalogo</h1>
+        </div>
+    );
 }
 
 export default Catalogo;
